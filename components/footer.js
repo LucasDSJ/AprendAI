f7cb98824799c36709bc62bd1807e9799f090a08
LucasDSJ <lucasdsjr2005@gmail.com>
2025-10-23 16:16:20 -0300

    Pequenos ajustes para o funcionamento do projeto.

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 const FooterComponent ={
     template: `
-    <footer class="footer text-center py-3 bg-primary text-white shadow">
+    <footer class="footer text-center py-3 mt-5 bg-primary text-white shadow">
         FalconProject © 2025
     </footer>
     `
